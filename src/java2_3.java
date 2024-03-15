b6aca2ab708447db060cdcc517c91ffc633dbbd6
kb8864 <yt7s858864@gmail.com>
2024-03-15 16:13:44 +0900

    javaプロジェクト作成

---
+++ src/java2_3.java
@@ -0,0 +1,14 @@
+/**
+ * java2_3
+ */
+public class java2_3 {
+
+  public static void main(String[] args) {
+    String str = "あいうえおかきくけこ";
+    char[] c = str.toCharArray();
+    for (int i = 0; i < c.length; i++) {
+      System.out.print(c[i]);
+    }
+  }
+
+}
